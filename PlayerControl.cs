8efed22597d9f36d37af8dbfd44881482d3a86e4
maxbehrendt1 <70982369+maxbehrendt1@users.noreply.github.com>
2020-10-31 15:24:27 -0400

    8 directional movement script

---
+++ PlayerControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+
+
+
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+
+        transform.position = transform.position + movement * Time.deltaTime * 4;
+    }
+}
