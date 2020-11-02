95d531caf22abe4d5d93b9b6e9a409f771104703
maxbehrendt1 <70982369+maxbehrendt1@users.noreply.github.com>
2020-11-02 17:33:01 -0500

    Add files via upload

--- PlayerControl.cs
+++ PlayerControl.cs
@@ -13,6 +13,19 @@ public class PlayerControl : MonoBehaviour
     {
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
 
-        transform.position = transform.position + movement * Time.deltaTime * 4;
+        transform.position = transform.position + movement * Time.deltaTime * 5;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Gem"))
+        {
+            Destroy(other.gameObject);
+        }
+
+        if (other.gameObject.CompareTag("Bone"))
+        {
+            Destroy(other.gameObject);
+        }
     }
 }
---
+++ ScoreManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour
+{
+
+    public static ScoreManager instance;
+    public TextMeshProUGUI gem_text;
+    public TextMeshProUGUI bone_text;
+    int gem_score;
+    int bone_score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public void ChangeGem(int gemValue)
+    {
+        gem_score += gemValue;
+        gem_text.text = "Gems: " + gem_score.ToString();
+    }
+
+    public void ChangeBone(int boneValue)
+    {
+        bone_score += boneValue;
+        bone_text.text = "Bones: " + bone_score.ToString();
+    }
+}
