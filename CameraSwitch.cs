28ae498f0c28bd518463b3c5ae8d95f0415d33b7
maxbehrendt1 <70982369+maxbehrendt1@users.noreply.github.com>
2020-12-01 17:02:26 -0500

    Switch cam

---
+++ CameraSwitch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraSwitch : MonoBehaviour
+{
+
+    public GameObject NormalCam;
+    public GameObject BossCam;
+   
+
+    public void OnTriggerEnter2D(Collider2D collider)
+    {
+        GameObject hitObj = collider.gameObject;
+
+        if (hitObj.tag == "Player")
+        {
+            NormalCam.SetActive(false);
+            BossCam.SetActive(true);
+        }
+    }
+}
