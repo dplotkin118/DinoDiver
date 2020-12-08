23296888d31c41b620043ae92a6b09350b5a32cb
maxbehrendt1 <70982369+maxbehrendt1@users.noreply.github.com>
2020-12-08 15:45:31 -0500

    Add files via upload

---
+++ MeleeAttack.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeAttack : MonoBehaviour
+{
+
+    public Animator animator;
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayer;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        // animator.SetTrigger("Melee");
+
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayer);
+
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("gottem")
+        }
+
+        void OnDrawGizmosSelected()
+        {
+            if (attackPoint == null)
+            {
+                return;
+            }
+
+            Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+        }
+    }
+}
